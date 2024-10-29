319fb9fb50cb80b75fe7f2af6b43c257de555db8
shedev633 <chloejanuslim@gmail.com>
2024-10-29 17:10:48 -0600

    Determine Game State

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -1,5 +1,17 @@
 # tic_tac_toe/logic/models.py
 
+WINNING_PATTERNS = (
+    "???......",
+    "...???...",
+    "......???",
+    "?..?..?..",
+    ".?..?..?.",
+    "..?..?..?",
+    "?...?...?",
+    "..?.?.?..",
+)
+
+
 import enum
 import re
 from dataclasses import dataclass
@@ -39,7 +51,49 @@ class Move:
     mark: Mark
     cell_index: int
     before_state: "GameState"
-    after_state:
+    after_state: "GameState"
+
+@dataclass(frozen=True)
+class GameState:
+    grid: Grid
+    starting_mark: Mark = Mark("X")
+
+@cached_property
+def current_mark(self) -> Mark:
+        if self.grid.x_count == self.grid.o_count:
+            return self.starting_mark
+        else:
+            return self.starting_mark.other
+
+@cached_property
+def game_not_started(self) -> bool:
+        return self.grid.empty_count == 9
 
 
+@cached_property
+def game_over(self) -> bool:
+    return self.winner is not None or self.tie
 
+@cached_property
+def tie(self) -> bool:
+    return self.winner is None and self.grid.empty_count == 0
+
+
+@cached_property
+def winner(self) -> Mark | None:
+        for pattern in WINNING_PATTERNS:
+            for mark in Mark:
+                if re.match(pattern.replace("?", mark), self.grid.cells):
+                    return mark
+        return None
+
+@cached_property
+def winning_cells(self) -> list[int]:
+        for pattern in WINNING_PATTERNS:
+            for mark in Mark:
+                if re.match(pattern.replace("?", mark), self.grid.cells):
+                    return [
+                        match.start()
+                        for match in re.finditer(r"\?", pattern)
+                    ]
+        return []
